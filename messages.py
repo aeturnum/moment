cf2145292e3456011a7d8796677088ab02963e6d
Daniel Drexler <ddrexler@bitcasa.com>
2015-03-23 07:53:24 -0700

    Adding actual tests and more utility functions to messages.

--- messages.py
+++ messages.py
@@ -1,124 +1,190 @@
 import time
+import json
+
+class EuphoriaServer(object):
+    def __init__(self, id, era):
+        self.id = id
+        self.era = era
+    def __eq__(self, other):
+        if hasattr(other, 'era') and hasattr(other, 'id'):
+            return self.id == other.id and self.era == other.era
 
 class EuphoriaUser(object):
-	def __init__(self, name, server_era, user_id, server_id):
-		self.name = name
-		self.id = user_id.split('-')[0]
-		self.session = user_id.split('-')[0]
-		self.server = {
-			'id':server_id,
-			'era':server_era
-		}
+    def __init__(self, name, server_era, user_id, server_id):
+        self.name = name
+        self.id = user_id.split('-')[0]
+        self.session = user_id.split('-')[1]
+        self.server = EuphoriaServer(server_id, server_era)
 
-	def __str__(self):
-		return '[{}]'.format(self.name)
+    def __str__(self):
+        return '[{}]'.format(self.name)
 
-	@staticmethod
-	def create(message):
-		u = EuphoriaUser(message['name'], message['server_era'], message['id'], message['server_id'])
-		return u
+    @staticmethod
+    def create(message):
+        u = EuphoriaUser(
+            message['name'],
+            message['server_era'],
+            message['id'],
+            message['server_id'])
+        return u
+
+    def __eq__(self, other):
+        attrs = ['name', 'id', 'session', 'server']
+        for attr in attrs:
+            if not hasattr(other, attr):
+                return False
+        return self.name == other.name\
+                and self.id == other.id \
+                and self.session == other.session \
+                and self.server == other.server
 
 class EuphoriaMessage(object):
-	def __init__(self, message_id, timestamp, sender, parent, content):
-		self.id = message_id
-		self.timestamp = int(timestamp)
-		self.sender = sender
-		self.parent = parent
-		self.content = content
+    def __init__(self, message_id, timestamp, sender, parent, content):
+        self.id = message_id
+        self.timestamp = int(timestamp)
+        self.sender = sender
+        self.parent = parent
+        self.content = content
 
-	def __lt__(self, other):
-		return self.timestamp < other.timestamp
+    def __lt__(self, other):
+        return self.timestamp < other.timestamp
 
-	def is_emote(self):
-		return self.content.find('/me') == 0
+    def is_emote(self):
+        return self.content.find('/me') == 0
 
-	def urwid_string(self, depth, index=None):
-		local_time = time.localtime(self.timestamp)
-		index_str = ''
-		if index:
-			index_str = '[{}]'.format(index)
-		if self.is_emote():
-			return '{}{}| {}<{}{}>'.format(index_str, time.strftime("%H:%M:%S", local_time), '| '*depth, self.sender.name, self.content.replace('/me', ''))
-		else:
-			return '{}{}| {}{}: {}'.format(index_str, time.strftime("%H:%M:%S", local_time), '| '*depth, self.sender, self.content)
+    def urwid_string(self, depth, index=None):
+        local_time = time.localtime(self.timestamp)
+        index_str = ''
+        if index:
+            index_str = '[{}]'.format(index)
+        if self.is_emote():
+            return '{}{}| {}<{}{}>'.format(
+                index_str,
+                time.strftime("%H:%M:%S", local_time),
+                '| '*depth,
+                self.sender.name,
+                self.content.replace('/me', ''))
+        else:
+            return '{}{}| {}{}: {}'.format(
+                index_str,
+                time.strftime("%H:%M:%S", local_time),
+                '| '*depth,
+                self.sender,
+                self.content)
 
-	def __str__(self):
-		local_time = time.localtime(self.timestamp)
-		return '{}|{}: {}'.format(time.strftime("%H:%M:%S", local_time), self.sender, self.content)
+    def __str__(self):
+        local_time = time.localtime(self.timestamp)
+        return '{}|{}: {}'.format(
+            time.strftime("%H:%M:%S", local_time),
+            self.sender,
+            self.content)
 
-	@staticmethod
-	def create(message):
-		if 'data' in message:
-			message = message['data']
+    @staticmethod
+    def create(message):
+        if 'data' in message:
+            message = message['data']
 
-		m = EuphoriaMessage(
-				message['id'],
-				message['time'],
-				EuphoriaUser.create(message['sender']),
-				message['parent'],
-				message['content'])
-		return m
+        m = EuphoriaMessage(
+                message['id'],
+                message['time'],
+                EuphoriaUser.create(message['sender']),
+                message['parent'],
+                message['content'])
+        return m
+
+    def __eq__(self, other):
+        attrs = ['id', 'timestamp', 'sender', 'parent', 'content']
+        for attr in attrs:
+            if not hasattr(other, attr):
+                return False
+        return self.id == other.id \
+               and self.timestamp == other.timestamp \
+               and self.sender == other.sender \
+               and self.parent == other.parent \
+               and self.content == other.content
 
 class EuphoriaPing(object):
-	def __init__(self, sent_time, next_ping):
-		self.sent_time = sent_time
-		self.next_time = next_ping
+    def __init__(self, sent_time, next_ping):
+        self.sent_time = sent_time
+        self.next_time = next_ping
 
-	def __str__(self):
-		return 'ping[{}]->{}'.format(self.sent_time, self.next_time)
+    def __repr__(self):
+        return str(self)
 
-	@staticmethod
-	def create(message):
-		if 'data' in message:
-			message = message['data']
-		p = EuphoriaPing(message['time'], message['next'])
+    def __str__(self):
+        return 'ping[{}]->{}'.format(self.sent_time, self.next_time)
 
-		return p
+    @staticmethod
+    def create(message):
+        if 'data' in message:
+            message = message['data']
+        p = EuphoriaPing(message['time'], message['next'])
+
+        return p
+
+    def __eq__(self, other):
+        attrs = ['sent_time', 'next_time']
+        for attr in attrs:
+            if not hasattr(other, attr):
+                return False
+        return self.sent_time == other.sent_time \
+               and self.next_time == other.next_time
 
 class EuphoriaSnapshot(object):
-	def __init__(self, version, session_id, log, listing):
-		self.version = version
-		self.session_id = session_id
-		self.log = log
-		self.listing = listing
-		self._users = None
-		self._messages = None
+    def __init__(self, version, session_id, log, listing):
+        self.version = version
+        self.session_id = session_id
+        self.log = log
+        self.listing = listing
+        self._users = None
+        self._messages = None
 
-	@property
-	def users(self):
-		if not self._users:
-			for user in self.listing:
-				add_user(user)
-		return self._users
+    @property
+    def users(self):
+        if not self._users:
+            self._users = []
+            for user in self.listing:
+                self._users.append(EuphoriaUser.create(user))
+        return self._users
 
-	@property
-	def messages(self):
-		if not self._users:
-			for user in self.listing:
-				add_user(user)
-		return self._users
+    @property
+    def messages(self):
+        if not self._messages:
+            self._messages = []
+            for message in self.log:
+                self._messages.append(EuphoriaMessage.create(message))
+        return self._users
 
-	@staticmethod
-	def create(message):
-		ss = EuphoriaSnapshot(
-			message['data']['version'],
-			message['data']['session_id'],
-			message['data']['log'], 
-			message['data']['listing']),
+    @staticmethod
+    def create(message):
+        ss = EuphoriaSnapshot(
+            message['data']['version'],
+            message['data']['session_id'],
+            message['data']['log'],
+            message['data']['listing']),
+        return ss
 
 def get_message_object(message):
-	type_to_class_map = {
-		'ping-event': EuphoriaPing,
-		'snapshot-event': EuphoriaSnapshot,
-		'send-event': EuphoriaMessage,
-		'send-reply': EuphoriaMessage, # do something about this?
-		'part-event': None
-	}
+    type_to_class_map = {
+        'ping-event': EuphoriaPing,
+        'snapshot-event': EuphoriaSnapshot,
+        'send-event': EuphoriaMessage,
+        'send-reply': EuphoriaMessage, # do something about this?
+        'part-event': None
+    }
 
-	if message['type'] in type_to_class_map:
-		type_class = type_to_class_map[message['type']]
-		if type_class:
-			return type_class.create(message)
+    if type(message) is not dict:
+        message = json.loads(message)
 
-	return None
+    print('message: {}'.format(message))
+    if 'type' in message:
+        if message['type'] in type_to_class_map:
+            type_class = type_to_class_map[message['type']]
+            if type_class:
+                return type_class.create(message)
 
+    else:
+        if 'id' in message and 'name' in message:
+            return EuphoriaUser.create(message)
+
+    return None
--- test_messages.py
+++ test_messages.py
@@ -1,15 +1,59 @@
 import unittest
+from messages import get_message_object, EuphoriaPing, EuphoriaUser
+import json
 
 class TestMessages(unittest.TestCase):
-	sample_data = [
-		'{"id":"","type":"ping-event","data":{"time":1426697160,"next":1426697190}}',
-		'{"id":"","type":"send-event","data":{"id":"00arxp1ofjhts","parent":"","time":1426698997,"sender":{"id":"e01253b4389655cf-0000011d","name":"mantis2","server_id":"heim.4","server_era":"00apdikn95eyo"},"content":"/me waves bye for a bit"}}',
-		'{"id":"17","type":"send-reply","data":{"id":"00astcu2uj0n4","parent":"00ast9qvv8w74","time":1426720292,"sender":{"id":"889f5ecba53ecdc7-0000004f","name":"Drex (ð¨)","server_id":"heim.1","server_era":"00as6ru6tjgn4"},"content":"gotcha :D"}}',
-		'{"id":"","type":"nick-event","data":{"id":"d81cb5df141851da-00000009","from":"","to":"Rubbery Negative Space"}}',
-	]
+    sample_ping = '{"id":"","type":"ping-event","data":{"time":1426697160,"next":1426697190}}'
+    sample_send = '{"id":"","type":"send-event","data":{"id":"00arxp1ofjhts","parent":"","time":1426698997,"sender":{"id":"d47f41e91c10b34a-00000008","name":"Drex","server_id":"heim.1","server_era":"00awjcddxlkhs"},"content":"/me waves bye for a bit"}}'
+    sample_reply = '{"id":"17","type":"send-reply","data":{"id":"00astcu2uj0n4","parent":"00ast9qvv8w74","time":1426720292,"sender":{"id":"889f5ecba53ecdc7-0000004f","name":"Drex (ð¨)","server_id":"heim.1","server_era":"00as6ru6tjgn4"},"content":"gotcha :D"}}'
+    sample_nick = '{"id":"","type":"nick-event","data":{"id":"d81cb5df141851da-00000009","from":"","to":"Rubbery Negative Space"}}'
+    sample_user = '{"id":"d47f41e91c10b34a-00000008","name":"Drex","server_id":"heim.1","server_era":"00awjcddxlkhs"}'
+    sample_snapshot = """
+    {"id":"","type":"snapshot-event",
+    "data":{"session_id":"889f5ecba53ecdc7-00000008","version":"054eb92661f5de57aa94d3de538fd5988f1caabe",
+    "listing":[{"id":"d47f41e91c10b34a-00000008","name":"Drex","server_id":"heim.1","server_era":"00awjcddxlkhs"},
+    {"id":"fe083605e43d3dbc-00000001","name":"chromakode","server_id":"heim.4","server_era":"00awje13l8y68"},
+    {"id":"4bd44ba37679d74d-0000000c","name":"intortus","server_id":"heim.3","server_era":"00awjd42wo0e8"}],
+    "log":[{"id":"00avxhybe3da8","parent":"00avwx7xstreo","time":1426795719,"sender":{"id":"889f5ecba53ecdc7-000000c9","name":"Drex (ð¨)","server_id":"heim.1","server_era":"00asububwunsw"},"content":"maybe there's an API that plugins can access - send message to server, create message, delete message, etc"},
+    {"id":"00avxi5qb8rgg","parent":"00avxhybe3da8","time":1426795723,"sender":{"id":"889f5ecba53ecdc7-000000c9","name":"Drex (ð¨)","server_id":"heim.1","server_era":"00asububwunsw"},"content":"that's common"},
+    {"id":"00avxi8cas7pc","parent":"00avwx7xstreo","time":1426795724,"sender":{"id":"4bd44ba37679d74d-0000008e","name":"intortus","server_id":"heim.3","server_era":"00asuckln9on4"},"content":"where \"norman in the sidebar\" can be expressed according to some standard"},
+    {"id":"00avxie5u2z28","parent":"00avwx7xstreo","time":1426795728,"sender":{"id":"fe083605e43d3dbc-000000ca","name":"chromakode","server_id":"heim.4","server_era":"00asudd8w5yww"},"content":"however, anything more specific than that will need to be custom"},
+    {"id":"00avxj9b00a9s","parent":"00avwx7xstreo","time":1426795743,"sender":{"id":"fe083605e43d3dbc-000000ca","name":"chromakode","server_id":"heim.4","server_era":"00asudd8w5yww"},"content":"oh totally, norman in the sidebar probably won't be a plugin per se-- a lot of the room customization stuff will be in the core featureset"}'"""
 
-	def setUp(self):
-		pass
+    def setUp(self):
+        pass
 
-	def tearDown(self):
-		pass
+    def test_ping_construction(self):
+        expected_sent = 1426697160
+        expected_next = 1426697190
+        ping  = EuphoriaPing.create(json.loads(self.sample_ping))
+        ping2 = get_message_object(self.sample_ping)
+        self.assertEqual(ping, ping)
+        self.assertEqual(ping, ping2)
+        self.assertEqual(ping.sent_time, expected_sent)
+        self.assertEqual(ping.next_time, expected_next)
+        self.assertEqual(ping2.sent_time, expected_sent)
+        self.assertEqual(ping2.next_time, expected_next)
+
+    def test_user_construction(self):
+        expected_username = 'Drex'
+        expected_id = 'd47f41e91c10b34a'
+        expected_session = '00000008'
+        expected_server_id = 'heim.1'
+        expected_server_era = '00awjcddxlkhs'
+        user = EuphoriaUser.create(json.loads(self.sample_user))
+        user2 = get_message_object(self.sample_user)
+        self.assertEqual(user, user)
+        self.assertEqual(user, user2)
+        users = [user, user2]
+        for u in users:
+            self.assertEqual(u.name, expected_username)
+            self.assertEqual(u.server.id, expected_server_id)
+            self.assertEqual(u.server.era, expected_server_era)
+            self.assertEqual(u.session, expected_session)
+            self.assertEqual(u.id, expected_id)
+
+    def test_message_construction(self):
+        expected_user = get_message_object(self.sample_user)
+    def tearDown(self):
+        pass
